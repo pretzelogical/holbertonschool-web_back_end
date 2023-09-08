e3dff57e1ac8b1a40a1de9bfa67d6bdffdfdf73a
pretzelogical <yingyangyo266@gmail.com>
2023-09-07 19:12:33 -0500

    2-5 start 6

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,28 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+}
