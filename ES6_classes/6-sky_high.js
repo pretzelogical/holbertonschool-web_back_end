52bca31e7b8e785b5a4b5ee8a102fd69e776fa83
pretzelogical <yingyangyo266@gmail.com>
2023-09-07 19:19:57 -0500

    6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,6 +3,18 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
+    this.floors = floors;
+  }
 
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
