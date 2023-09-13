fc14209af1a4788155e5fd145d33a36cbc7795e1
pretzelogical <yingyangyo266@gmail.com>
2023-09-12 19:47:00 -0500

    add 9 & 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(map) {
+  const quantity = [];
+  if (!(map instanceof Map)) {
+    throw new Error("Cannot proccss");
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      quantity.push(key);
+    }
+  });
+  quantity.forEach((q) => map.set(q, 100));
+}
