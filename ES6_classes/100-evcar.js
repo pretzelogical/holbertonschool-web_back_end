ec13119756764e7faeb203664d4cbc294d34f5c3
pretzelogical <yingyangyo266@gmail.com>
2023-09-12 21:48:56 -0500

    add 100

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,29 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  get range() {
+    return this._range;
+  }
+}
