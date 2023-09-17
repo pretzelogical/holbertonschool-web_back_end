77a1210b8e3989fb2118cb8afcce56c93d7bd0ec
pretzelogical <yingyangyo266@gmail.com>
2023-09-17 12:58:05 -0500

    do 0-3 in ES6_promise

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let logString = "";
+  uploadPhoto()
+    .then((res) => {
+      logString += res.body;
+      return createUser();
+    })
+    .then((res) => {
+      logString += ` ${res.firstName} ${res.lastName}`;
+      console.log(logString);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
